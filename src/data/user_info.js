905e9dd8bf2a5f2ade9d4e59eb8e902a402e9d11
bastienyoussfi <bastien.youssfi@gmail.com>
2024-06-04 20:30:52 +0900

    Portfolio updated

---
+++ src/data/user_info.js
@@ -0,0 +1,82 @@
+const info = {
+    main: {
+        name: "Bastien Youssfi",
+        description: "I am a software engineer with expertise in software development and AI. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Software Engineer",
+        photo: "../photo.jpg",
+        email: "bastien.youssfi@gmail.com",
+        tracking_id: "G-BNTTBGHXSC"
+    },
+
+    socials: {
+		github: "https://github.com/bastienyoussfi",
+		linkedin: "https://linkedin.com/in/bastienyoussfi",
+		instagram: "https://instagram.com/bastienyoussfi",
+    },
+
+    skills: {
+        languages: {
+            description: "I possess proficient expertise in a diverse range of programming languages, including C, C++, C#, Python and Java.",
+            image: "https://skillicons.dev/icons?i=c,cpp,cs,py,java&perline=6&theme="
+        },
+        frameworks: {
+            description: "Experienced in a versatile tech stack, including React, Express.js, Node.js, PostgreSQL, Tailwind CSS, and PyTorch.",
+            image: "https://skillicons.dev/icons?i=react,express,nodejs,postgres,tailwind,pytorch&perline=6&theme="
+        },
+        tools: {
+            description: "I am well-versed in utilizing various tools and platforms to enhance the development process, including Git, Github, Netlify, Vercel, Firebase, and Postman.",
+            image: "https://skillicons.dev/icons?i=git,github,netlify,vercel,firebase,postman&perline=6&theme="
+        },
+    },
+
+    projects: [
+        {
+			title: "Physics Engine",
+			description: "Scalable physics engine first made to simulate simple particles interaction using Verlet Integration method.",
+			technologies: "https://skillicons.dev/icons?i=cpp,visualstudio,github,0,0&perline=5&theme=",
+			github: "https://github.com/bastienyoussfi/PhysicsEngine",
+            link: "https://github.com/bastienyoussfi/PhysicsEngine"
+		},
+        {
+            title: "Notes App",
+            description: "Simple notes app made using React and Node.js to store and manage notes.",
+            technologies: "https://skillicons.dev/icons?i=react,nodejs,express,github,0&perline=5&theme=",
+            github: "",
+            link: "",
+        },
+        {
+            title: "Portfolio",
+            description: "Portfolio website ",
+            technologies: "https://skillicons.dev/icons?i=react,github,tailwind,0&perline=5&theme=",
+            github: "",
+            link: "",
+        }
+        
+    ],
+
+    education: [
+        {
+            school: "Grenoble INP-ENSIMAG",
+            degree: "Engineer Degree in Computer Science",
+            year: "2020 - Present",
+            image: "ensimag.png",
+            link: "https://ensimag.grenoble-inp.fr/"
+        },
+        {
+            school: "University of Tsukuba",
+            degree: "Graduate School Information Science and Engineering Degree Program",
+            year: "2023 - 2024",
+            image: "tsukuba.png",
+            link: "https://www.tsukuba.ac.jp/en/"
+        },
+    ],
+
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at bastien.youssfi@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    footer: "© 2024 Bastien Youssfi. All Rights Reserved"
+}
+
+export default info
